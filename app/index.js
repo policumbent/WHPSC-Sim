b6c41bf8d0c68ca6adde8ce50e2469ecdfc67d5e
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-10-19 23:20:19 +0200

    npm run publish

--- app/index.js
+++ app/index.js
@@ -35,7 +35,7 @@ const createWindow = async () => {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
     window = new BrowserWindow({
         width: Math.round(width * 2/3),
-        height: Math.round(height * 4/5),
+        height: Math.round(height * 0.8),
         webPreferences: {
             experimentalFeatures: true,
             nodeIntegration: true
