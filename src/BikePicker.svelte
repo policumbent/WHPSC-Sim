c6a4f8bac89197e69ff71d72cb368f5ba03c329f
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-10-23 11:04:03 +0200

    new readme

--- src/BikePicker.svelte
+++ src/BikePicker.svelte
@@ -18,7 +18,7 @@
     new Bike("Coral", "img/recumbent.svg", "A recumbent bike",
             new BikeSettings(14, 0.091, 1.450, 0.97, 0.32+getAreaCorrection(), 0.57, 'coefficients_normal_bike_4.json')),
     new Bike("Road Bike", "img/normal.svg", "A road bike",
-            new BikeSettings(7.5, 0.0786, 2.105, 0.98, 0.43+getAreaCorrection()*0.8, 0.67, 'coefficients_normal_bike_4.json'))
+            new BikeSettings(7.5, 0.0786, 2.105, 0.98, 0.43+getAreaCorrection()*0.7, 0.67, 'coefficients_normal_bike_4.json'))
   ];
 
   function getAreaCorrection() {
